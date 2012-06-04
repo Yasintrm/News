93e2b1754ed60121623177bc33bf4b89881d6925
Yasin Tarim <yasintarim@Yasins-MacBook.local>
2012-06-03 22:45:59 -0700

    Initial Commit

---
+++ News/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by Yasin Tarim on 6/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
